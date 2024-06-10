f3281df4d87eb8b501f47b3c5be602c6a3a8b630
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-06-10 17:59:51 +0600

    test

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,20 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
 
-const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
-    res.end()
-  }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
-  }
-  // 404
-  else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
-  }
+const app = express()
+
+// setup static and middleware
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
 })
 
-server.listen(5000)
+app.listen(5000, () => {
+  console.log('server is listening on port 5000....')
+})
--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
+  console.log(req.method)
   const url = req.url
   // home page
   if (url === '/') {
